0a8bfb2536585339e5511508482112c97df9e34b
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-04-19 18:06:50 -0700

    Footer: remove absolute positioning

--- src/Footer.ts
+++ src/Footer.ts
@@ -1,6 +1,6 @@
 import { v } from '@dojo/widget-core/d';
 import { DNode } from '@dojo/widget-core/interfaces';
-import { RegistryMixin, RegistryMixinProperties }  from '@dojo/widget-core/mixins/Registry';
+import { RegistryMixin, RegistryMixinProperties } from '@dojo/widget-core/mixins/Registry';
 import { theme, ThemeableMixin, ThemeableProperties } from '@dojo/widget-core/mixins/Themeable';
 import WidgetBase from '@dojo/widget-core/WidgetBase';
 
