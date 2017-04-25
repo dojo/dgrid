c6e77d6eedc94857f60e62f10acf047bde0fd191
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-04-24 21:43:39 -0700

    Pagination: update unit tests to use harness

--- tests/unit/Pagination.ts
+++ tests/unit/Pagination.ts
@@ -1,9 +1,10 @@
 import { assert } from 'chai';
+import harness, { assignProperties, assignChildProperties, Harness, replaceChild } from '@dojo/test-extras/harness';
 import { v } from '@dojo/widget-core/d';
 import { VNode } from '@dojo/interfaces/vdom';
 import * as registerSuite from 'intern/lib/interfaces/object';
-import Pagination from '../../src/Pagination';
-import * as paginationCss from '../../src/styles/pagination.m.css';
+import Pagination, { PaginationProperties } from '../../src/Pagination';
+import * as css from '../../src/styles/pagination.m.css';
 
 const items = (function () {
 	const items = [];
@@ -18,12 +19,21 @@ const items = (function () {
 	return items;
 })();
 
+let widget: Harness<PaginationProperties, typeof Pagination>;
+
 registerSuite({
 	name: 'Pagination',
 
+	beforEach() {
+		widget = harness(Pagination);
+	},
+
+	afterEach() {
+		widget.destroy();
+	},
+
 	render: {
 		'single page'() {
-			const widget = new Pagination();
 			widget.setProperties({
 				items,
 				pagination: {
@@ -35,19 +45,18 @@ registerSuite({
 				}
 			});
 
-			const vnode = <VNode> widget.__render__();
-			assert.strictEqual(vnode.vnodeSelector, 'div');
-			assert.isTrue(vnode.properties!.classes![paginationCss.pagination]);
-			assert.strictEqual(vnode.children!.length, 1, 'Only status node should be displayed');
-
-			const statusNode = vnode.children![0];
-			assert.strictEqual(statusNode.text, `1 - ${items.length} of ${items.length} results`);
-			assert.isTrue(statusNode.properties!.classes![paginationCss.status]);
+			widget.expectRender(v('div', {
+				classes: widget.classes(css.pagination)
+			}, [
+				v('span', {
+					classes: widget.classes(css.status)
+				}, [ `1 - ${items.length} of ${items.length} results` ])
+			]));
 		},
 
 		'multiple pages'() {
 			const itemsPerPage = 5;
-			const widget = new Pagination();
+
 			widget.setProperties({
 				items,
 				pagination: {
@@ -59,32 +68,33 @@ registerSuite({
 				}
 			});
 
-			const vnode = <VNode> widget.__render__();
-			const statusNode = vnode.children![0];
-			assert.strictEqual(statusNode.text, `1 - ${itemsPerPage} of ${items.length} results`);
+			widget.expectRender(v('div', {
+				classes: widget.classes(css.pagination)
+			}, [
+				v('span', {
+					classes: widget.classes(css.status)
+				}, [ `1 - ${itemsPerPage} of ${items.length} results` ]),
+				v('span', {
+					classes: widget.classes(css.navigation)
+				}, [
+					v('span', {
+						classes: widget.classes(css.pageLink, css.arrow)
+					}, [ '‹' ]),
+					v('span', {
+						classes: widget.classes(css.pageLinks)
+					}), // TODO: children
+					v('span', {
+						classes: widget.classes(css.pageLink, css.arrow)
+					}, [ '›' ])
+				])
+			]));
 
-			const navigationNode = vnode.children![1];
-			assert.isTrue(navigationNode.properties!.classes![paginationCss.navigation]);
-			assert.strictEqual(navigationNode.children!.length, 3, 'navigation node should have 3 children');
-
-			const previousLink = navigationNode.children![0];
-			assert.isTrue(previousLink.properties!.classes![paginationCss.pageLink]);
-			assert.isTrue(previousLink.properties!.classes![paginationCss.arrow]);
-			assert.strictEqual(previousLink.text, '‹');
-
-			const pageLinks = navigationNode.children![1];
-			assert.isTrue(pageLinks.properties!.classes![paginationCss.pageLinks]);
-			assert.strictEqual(pageLinks.children!.length, 2, 'pageLinks node should have 2 children');
-
-			const nextLink = navigationNode.children![2];
-			assert.isTrue(nextLink.properties!.classes![paginationCss.pageLink]);
-			assert.isTrue(nextLink.properties!.classes![paginationCss.arrow]);
-			assert.strictEqual(nextLink.text, '›');
+			// assert.strictEqual(pageLinks.children!.length, 2, 'pageLinks node should have 2 children');
 		},
 
 		'middle page'() {
 			const itemsPerPage = 1;
-			const widget = new Pagination();
+
 			widget.setProperties({
 				items,
 				pagination: {
@@ -96,53 +106,61 @@ registerSuite({
 				}
 			});
 
-			const vnode = <VNode> widget.__render__();
-			console.dir(vnode);
-			const pageLinks = vnode.children![1].children![1];
-			assert.strictEqual(pageLinks.children!.length, 9, 'pageLinks node should have 9 children');
-
-			assert.strictEqual(pageLinks.children![0].properties!.tabindex, '0');
-			assert.strictEqual(pageLinks.children![0].text, '1');
-			assert.isTrue(pageLinks.children![0].properties!.classes![paginationCss.pageLink]);
-			assert.notOk(pageLinks.children![0].properties!.classes![paginationCss.disabled]);
-
-			assert.strictEqual(pageLinks.children![1].text, '...');
-			assert.notOk(pageLinks.children![1].properties!.classes![paginationCss.pageLink]);
-			assert.isTrue(pageLinks.children![1].properties!.classes![paginationCss.pageSkip]);
-
-			assert.strictEqual(pageLinks.children![2].properties!.tabindex, '0');
-			assert.strictEqual(pageLinks.children![2].text, '3');
-			assert.isTrue(pageLinks.children![2].properties!.classes![paginationCss.pageLink]);
-			assert.notOk(pageLinks.children![2].properties!.classes![paginationCss.disabled]);
-
-			assert.strictEqual(pageLinks.children![3].properties!.tabindex, '0');
-			assert.strictEqual(pageLinks.children![3].text, '4');
-			assert.isTrue(pageLinks.children![3].properties!.classes![paginationCss.pageLink]);
-			assert.notOk(pageLinks.children![3].properties!.classes![paginationCss.disabled]);
-
-			assert.strictEqual(pageLinks.children![4].properties!.tabindex, '-1');
-			assert.strictEqual(pageLinks.children![4].text, '5');
-			assert.isTrue(pageLinks.children![4].properties!.classes![paginationCss.pageLink]);
-			assert.isTrue(pageLinks.children![4].properties!.classes![paginationCss.disabled]);
-
-			assert.strictEqual(pageLinks.children![5].properties!.tabindex, '0');
-			assert.strictEqual(pageLinks.children![5].text, '6');
-			assert.isTrue(pageLinks.children![5].properties!.classes![paginationCss.pageLink]);
-			assert.notOk(pageLinks.children![5].properties!.classes![paginationCss.disabled]);
-
-			assert.strictEqual(pageLinks.children![6].properties!.tabindex, '0');
-			assert.strictEqual(pageLinks.children![6].text, '7');
-			assert.isTrue(pageLinks.children![6].properties!.classes![paginationCss.pageLink]);
-			assert.notOk(pageLinks.children![6].properties!.classes![paginationCss.disabled]);
-
-			assert.strictEqual(pageLinks.children![7].text, '...');
-			assert.notOk(pageLinks.children![7].properties!.classes![paginationCss.pageLink]);
-			assert.isTrue(pageLinks.children![7].properties!.classes![paginationCss.pageSkip]);
-
-			assert.strictEqual(pageLinks.children![8].properties!.tabindex, '0');
-			assert.strictEqual(pageLinks.children![8].text, '10');
-			assert.isTrue(pageLinks.children![8].properties!.classes![paginationCss.pageLink]);
-			assert.notOk(pageLinks.children![8].properties!.classes![paginationCss.disabled]);
+			widget.expectRender(v('div', {
+				classes: widget.classes(css.pagination)
+			}, [
+				v('span', {
+					classes: widget.classes(css.status)
+				}, [ `1 - ${itemsPerPage} of ${items.length} results` ]),
+				v('span', {
+					classes: widget.classes(css.navigation)
+				}, [
+					v('span', {
+						classes: widget.classes(css.pageLink, css.arrow)
+					}, [ '‹' ]),
+					v('span', {
+						classes: widget.classes(css.pageLinks)
+					}, [
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: 0
+						}, [ '1' ]),
+						v('span', {
+							classes: widget.classes(css.pageSkip)
+						}, [ '...' ]),
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: 0
+						}, [ '3' ]),
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: 0
+						}, [ '4' ]),
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: -1
+						}, [ '5' ]),
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: 0
+						}, [ '6' ]),
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: 0
+						}, [ '7' ]),
+						v('span', {
+							classes: widget.classes(css.pageSkip)
+						}, [ '...' ]),
+						v('span', {
+							classes: widget.classes(css.pageLink),
+							tabIndex: 0
+						}, [ '10' ])
+					]),
+					v('span', {
+						classes: widget.classes(css.pageLink, css.arrow)
+					}, [ '›' ])
+				])
+			]));
 		}
 	}
 });
