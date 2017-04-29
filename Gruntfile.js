cd8b9d0334d9504411002590c663961214c2f815
Mangala Sadhu Sangeet Singh Khalsa <mssskhalsa@gmail.com>
2017-04-28 21:38:51 -0700

    Add examples folder and HTML page (#29)
    * Add examples folder and HTML page

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,20 @@ module.exports = function (grunt) {
                 cwd: 'src',
                 src: 'styles/dgrid.css',
                 dest: '<%= distDirectory %>'
+            },
+            examples: {
+                expand: true,
+                cwd: '.',
+                src: 'examples/**/*.{html,css,json,xml,js,txt}',
+                dest: '<%= devDirectory %>'
             }
         }
     });
 
     grunt.registerTask('dev', grunt.config.get('devTasks').concat([
         'postcss:modules-dev',
-        'copy:devStyles'
+        'copy:devStyles',
+        'copy:examples'
     ]));
 
     grunt.registerTask('dist', grunt.config.get('distTasks').concat([
