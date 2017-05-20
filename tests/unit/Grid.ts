c58d9d276efb985565d01c5c30d9f7283d6bc110
Neil Roberts <neil@onebahamas.com>
2017-05-20 16:15:31 -0400

    Restore changes accidentally removed from the Grid unit test

--- tests/unit/Grid.ts
+++ tests/unit/Grid.ts
@@ -1,44 +1,79 @@
 import * as registerSuite from 'intern!object';
 
-import { compareProperty } from '@dojo/test-extras/support/d';
 import harness, { Harness } from '@dojo/test-extras/harness';
+import { assignChildProperties, compareProperty } from '@dojo/test-extras/support/d';
 import { v, w } from '@dojo/widget-core/d';
-import RegistryHandler from '@dojo/widget-core/RegistryHandler';
-import WidgetRegistry from '@dojo/widget-core/WidgetRegistry';
 
 import Body from '../../src/Body';
-import Cell from '../../src/Cell';
 import Grid, { GridProperties } from '../../src/Grid';
+import GridRegistry, { gridRegistry } from '../../src/GridRegistry';
 import Header from '../../src/Header';
-import HeaderCell from '../../src/HeaderCell';
-import { Column } from '../../src/interfaces';
-import Row from '../../src/Row';
+import { Column, ItemProperties, SortDetails } from '../../src/interfaces';
 import ArrayDataProvider from '../../src/providers/ArrayDataProvider';
 import * as css from '../../src/styles/grid.m.css';
+import DataProviderBase from '../../src/bases/DataProviderBase';
 
-let widget: Harness<GridProperties, typeof Grid>;
-
-const compareRegistryProperty: WidgetRegistry = <any> compareProperty((value) => {
-	if (value instanceof RegistryHandler) {
-		return value.has('header') &&
+const compareRegistryProperty: GridRegistry = <any> compareProperty((value) => {
+	if (value instanceof GridRegistry) {
+		return value.has('body') &&
+			value.has('cell') &&
+			value.has('footer') &&
+			value.has('header') &&
 			value.has('header-cell') &&
-			value.has('body') &&
-			value.has('row') &&
-			value.has('cell');
+			value.has('row');
 	}
 	return false;
 });
 
 const columns: Column<any>[] = [
-	{ id: 'foo', label: 'foo' }
+	{ id: 'name', label: 'Name' }
 ];
 
-const registry = new WidgetRegistry();
-registry.define('header', Header);
-registry.define('header-cell', HeaderCell);
-registry.define('body', Body);
-registry.define('row', Row);
-registry.define('cell', Cell);
+const items = [
+	{
+		id: 1,
+		name: 'One'
+	},
+	{
+		id: 2,
+		name: 'Two'
+	}
+];
+
+const itemProperties: ItemProperties<any>[] = [
+	{
+		id: '1',
+		data: items[0]
+	},
+	{
+		id: '2',
+		data: items[1]
+	}
+];
+
+const items2 = [
+	{
+		id: 3,
+		name: 'Three'
+	},
+	{
+		id: 4,
+		name: 'Four'
+	}
+];
+
+const itemProperties2: ItemProperties<any>[] = [
+	{
+		id: '3',
+		data: items2[0]
+	},
+	{
+		id: '4',
+		data: items2[1]
+	}
+];
+
+let widget: Harness<GridProperties, typeof Grid>;
 
 registerSuite({
 	name: 'Grid',
@@ -54,9 +89,36 @@ registerSuite({
 	'dgrid'() {
 		widget.setProperties({
 			dataProvider: new ArrayDataProvider({
-				data: []
+				data: items
 			}),
 			columns
+		});
+
+		widget.expectRender(v('div', {
+			classes: widget.classes(css.grid),
+			role: 'grid'
+		}, [
+			w<Header>('header', {
+				columns,
+				registry: compareRegistryProperty,
+				sortDetails: [],
+				theme: undefined,
+				onSortRequest: widget.listener
+			}),
+			w<Body>('body', {
+				columns,
+				items: itemProperties,
+				registry: compareRegistryProperty,
+				theme: undefined
+			})
+		]));
+	},
+
+	'dgrid no dataProvider'() {
+		widget.setProperties({
+			dataProvider: new (class extends DataProviderBase {})({}),
+			columns,
+			registry: gridRegistry
 		});
 
 		widget.expectRender(v('div', {
@@ -78,37 +140,100 @@ registerSuite({
 			})
 		]));
 	},
+
 	'sort'() {
 		const properties: GridProperties = {
 			dataProvider: new ArrayDataProvider({
-				data: []
+				data: items
 			}),
-			columns: [
-				{ id: 'foo', label: 'foo' }
-			]
+			columns
 		};
 
-		properties.dataProvider.sort({
-			columnId: 'foo',
+		widget.setProperties(properties);
+
+		const expected = v('div', {
+			classes: widget.classes(css.grid),
+			role: 'grid'
+		}, [
+			w<Header>('header', {
+				columns,
+				registry: compareRegistryProperty,
+				sortDetails: [],
+				theme: undefined,
+				onSortRequest: widget.listener
+			}),
+			w<Body>('body', {
+				columns,
+				items: itemProperties,
+				registry: compareRegistryProperty,
+				theme: undefined
+			})
+		]);
+
+		widget.expectRender(expected);
+
+		const sortDetails: SortDetails = {
+			columnId: 'name',
 			direction: 'desc'
+		};
+
+		properties.dataProvider.sort(sortDetails);
+
+		assignChildProperties(expected, 0, {
+			sortDetails: [ sortDetails ]
 		});
 
+		assignChildProperties(expected, 1, {
+			items: [ itemProperties[1], itemProperties[0] ]
+		});
+
+		widget.expectRender(expected);
 	},
+
 	'reassign dataProvider'() {
 		let properties: GridProperties = {
 			dataProvider: new ArrayDataProvider({
-				data: []
+				data: items
 			}),
-			columns: [
-				{ id: 'foo', label: 'foo' }
-			]
+			columns
 		};
+
+		widget.setProperties(properties);
+
+		const expected = v('div', {
+			classes: widget.classes(css.grid),
+			role: 'grid'
+		}, [
+			w<Header>('header', {
+				columns,
+				registry: compareRegistryProperty,
+				sortDetails: [],
+				theme: undefined,
+				onSortRequest: widget.listener
+			}),
+			w<Body>('body', {
+				columns,
+				items: itemProperties,
+				registry: compareRegistryProperty,
+				theme: undefined
+			})
+		]);
+
+		widget.expectRender(expected);
 
 		properties = {
 			...properties,
 			dataProvider: new ArrayDataProvider({
-				data: []
+				data: items2
 			})
 		};
+
+		assignChildProperties(expected, 1, {
+			items: itemProperties2
+		});
+
+		widget.setProperties(properties);
+
+		widget.expectRender(expected);
 	}
 });
