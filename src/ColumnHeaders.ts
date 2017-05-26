72d47b0551d8c048cadc2ab6669033f7992bb6f4
Neil Roberts <neil@onebahamas.com>
2017-05-26 08:51:22 -0500

    Alphebetize imports

--- src/ColumnHeaders.ts
+++ src/ColumnHeaders.ts
@@ -1,7 +1,7 @@
 import { v, w } from '@dojo/widget-core/d';
 import { DNode } from '@dojo/widget-core/interfaces';
 import { RegistryMixin, RegistryMixinProperties }  from '@dojo/widget-core/mixins/Registry';
-import { ThemeableMixin, theme, ThemeableProperties } from '@dojo/widget-core/mixins/Themeable';
+import { theme, ThemeableMixin, ThemeableProperties } from '@dojo/widget-core/mixins/Themeable';
 import WidgetBase from '@dojo/widget-core/WidgetBase';
 import ColumnHeaderCell from './ColumnHeaderCell';
 import { HasColumns, HasSortDetails, HasSortEvent } from './interfaces';
